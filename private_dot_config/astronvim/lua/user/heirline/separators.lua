50cc7bfa219f8b815b01d5ae115f14201d21ea36
Ben Woodward <ben@benwoodward.me.uk>
2023-05-12 23:19:16 +0100

    Heirline fixes

--- private_dot_config/astronvim/lua/user/heirline/separators.lua
+++ private_dot_config/astronvim/lua/user/heirline/separators.lua
@@ -1,5 +1,5 @@
 return {
-  left = { "", " " }, -- separator for the left side of the statusline
-  right = { " ", "" }, -- separator for the right side of the statusline
-  tab = { "", "" },
+	left = { "", " " }, -- separator for the left side of the statuslie
+	right = { " ", "" }, -- separator for the right side of the statusline
+	tab = { "", "" },
 }
