dfc81da40235af63e2727d213b5da70962a48b94
Ben Woodward <ben@benwoodward.me.uk>
2023-06-02 18:59:19 +0100

    astronvim updates

---
+++ private_dot_config/astronvim/lua/user/options.lua
@@ -0,0 +1,6 @@
+return {
+	g = {
+		diagnostics_mode = 1,
+		python3_host_prog = "~/.pyenv/versions/neovim/bin/python",
+	},
+}
--- private_dot_config/astronvim/lua/user/polish.lua
+++
@@ -1,5 +0,0 @@
-return function()
-	vim.g.python3_host_prog = "~/.pyenv/versions/neovim/bin/python"
-
-	vim.g.diagnostics_mode = 1 -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-end
