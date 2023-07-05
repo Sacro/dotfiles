c26097070f4d25e1a7e53c7bea6c7485027e6f3e
Ben Woodward <ben@benwoodward.me.uk>
2023-07-05 14:29:22 +0100

    Migrate rainbow brackets

--- private_dot_config/astronvim/lua/user/plugins/community/editing-support.lua
+++ private_dot_config/astronvim/lua/user/plugins/community/editing-support.lua
@@ -1,6 +1,6 @@
 return {
 	{ import = "astrocommunity.editing-support.neogen" },
-	{ import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+	{ import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 	{ import = "astrocommunity.editing-support.refactoring-nvim" },
 	{ import = "astrocommunity.editing-support.suda-vim" },
 	{ import = "astrocommunity.editing-support.todo-comments-nvim" },
