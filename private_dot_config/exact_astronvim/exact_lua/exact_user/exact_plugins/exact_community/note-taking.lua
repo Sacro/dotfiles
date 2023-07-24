a52e4f74b11b65c317d381a56f4c159fac3bcbec
Ben Woodward <ben@benwoodward.me.uk>
2023-07-24 15:18:15 +0100

    Updates

--- private_dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/exact_community/note-taking.lua
+++ private_dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/exact_community/note-taking.lua
@@ -1,3 +1,12 @@
+local obsidian_path = "/Documents/Obsidian"
+
 return {
-	{ import = "astrocommunity.note-taking.neorg" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  {
+    "obsidian.nvim",
+    opts = {
+      dir = vim.env.HOME .. obsidian_path,
+    },
+    event = { "BufReadPre  *" .. obsidian_path .. "/*.md" },
+  },
 }
