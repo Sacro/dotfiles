8f8875c8a769d5d3666472eaa17eab03da5e2e2f
Ben Woodward <ben@benwoodward.me.uk>
2023-08-19 14:08:16 +0100

    Updates

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -22,21 +22,14 @@ config.hide_tab_bar_if_only_one_tab = true
 
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("JetBrainsMonoNL NFM")
-config.font_size = 16
 
-local default_prog = nil
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
+config.font_size = 14
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	-- Configs for Windows only
-	-- font_dirs = {
-	--     'C:\\Users\\whoami\\.dotfiles\\.fonts'
-	-- }
-	-- default_prog = { "wsl.exe", "~", "-d", "Ubuntu-20.04" }
-	default_prog = { "pwsh.exe" }
-end
+-- config.font = wezterm.font("FantasqueSansM Nerd Font Mono")
+-- config.font_size = 15
 
-config.default_prog = default_prog
+config.default_prog = (wezterm.target_triple == "x86_64-pc-windows-msvc" and "pwsh.exe" or nil)
 
 -- Here comes the Neovim configuration
 --
@@ -91,6 +84,11 @@ config.keys = {
 	split_nav("resize", "k"),
 	split_nav("resize", "l"),
 }
+
+local configAvailable, localConfig = pcall(require, "local")
+if configAvailable then
+	localConfig.apply_to_config(config)
+end
 
 -- and finally, return the configuration to wezterm
 return config
