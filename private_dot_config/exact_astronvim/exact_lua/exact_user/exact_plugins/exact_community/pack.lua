5670172a66b4386a1dafa834eb0801b548e3dd73
Ben Woodward <ben@benwoodward.me.uk>
2023-12-11 21:53:50 +0000

    Add justfile pack

--- private_dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/exact_community/pack.lua
+++ private_dot_config/exact_astronvim/exact_lua/exact_user/exact_plugins/exact_community/pack.lua
@@ -11,6 +11,7 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.just" },
   { import = "astrocommunity.pack.kotlin" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
