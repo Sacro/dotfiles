4ae26449b46073a2d89665bca02f3bced32cebc2
Ben Woodward <ben@benwoodward.me.uk>
2024-02-23 21:43:28 +0000

    feat(nvim): enable theme and lua pack

--- private_dot_config/exact_nvim/exact_lua/community.lua
+++ private_dot_config/exact_nvim/exact_lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,6 +6,12 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
+
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  -- pack
   { import = "astrocommunity.pack.lua" },
+
   -- import/override with your plugins folder
 }
