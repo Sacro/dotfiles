f64a02bfb1d22e8984dc1646cc8d72d430563276
Ben Woodward <ben@benwoodward.me.uk>
2024-02-23 18:15:35 +0000

    update astronvim template

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
@@ -7,11 +7,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(
-      opts.ensure_installed,
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
-      "vim"
+      "vim",
       -- add more arguments for adding more treesitter parsers
-    )
+    })
   end,
 }
