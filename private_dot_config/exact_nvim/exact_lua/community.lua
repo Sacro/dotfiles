b2ef3d2ba15b2dc61c9f708525e4b3303276053c
Ben Woodward <ben@benwoodward.me.uk>
2024-04-01 15:23:26 +0100

    feat: more nvim

--- private_dot_config/exact_nvim/exact_lua/community.lua
+++ private_dot_config/exact_nvim/exact_lua/community.lua
@@ -79,6 +79,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.pack.ps1" },
