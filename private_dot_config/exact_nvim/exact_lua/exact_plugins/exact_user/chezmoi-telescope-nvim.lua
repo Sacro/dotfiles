15e14fabf773ae644edb1d6f2b9d6c38861d5a98
Ben Woodward <ben@benwoodward.me.uk>
2024-07-17 14:31:29 +0100

    fix(nvim): remove chezmoi-telescope

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_user/chezmoi-telescope-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      { "GianniBYoung/chezmoi-telescope.nvim" },
-    },
-    config = function() require("telescope").load_extension "chezmoi" end,
-  },
-}
