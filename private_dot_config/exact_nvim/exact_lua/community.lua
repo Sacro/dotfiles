1948240f242bf732f0af6afb867fd9f2caa9813a
Ben Woodward <ben@benwoodward.me.uk>
2026-06-05 12:04:47 +0100

    fix(nvim): readd some bits

--- private_dot_config/exact_nvim/exact_lua/community.lua
+++ private_dot_config/exact_nvim/exact_lua/community.lua
@@ -10,5 +10,11 @@ return {
     -- dev = true
   },
 
+  -- bars and lines
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+
   { import = "astrocommunity.colorscheme.catppuccin" },
+
+  { import = "astrocommunity.pack.go" },
 }
